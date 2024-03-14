953b213815a313b2de9d12de50ccc8821c25b0a0
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-14 09:37:53 +0100

    one way data binding

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name: string = 'Ironman';
+  public age: number = 35;
 
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(change : string):void{
+    this.name = change;
+  }
+
+  changeAge(change : number):void{
+    this.age = change;
+  }
 }
